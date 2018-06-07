5fc8f5af175adfecc490a48e58ad204ac3b9a758
LunaCI <lunaci@luadist.org>
2018-06-07 23:46:16 +0000

    Update to version 1.0-1

---
+++ busted.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+for /f "delims=" %%i in ('cd') do set cwd=%%i
+for %%X in (lua.exe) do (set FOUND=%%~$PATH:X)
+if defined FOUND (
+  set cmd="lua"
+) else (
+  for %%X in (luajit.exe) do (set FOUND=%%~$PATH:X)
+  if defined FOUND (
+    set cmd="luajit"
+  )
+)
+if "%cmd%"=="" (
+  echo "Busted requires that a valid execution environment be specified(or that you have lua or luajit accessible in your PATH). Aborting."
+) else (
+  if "%*"=="--help" set TRUE=1
+  if "%*"=="--version" set TRUE=1
+  if defined TRUE  (
+    pushd %~dp0 && (call "%cmd%" bootstrap %*) && popd
+  ) else (
+    pushd %~dp0 && (call "%cmd%" bootstrap --cwd="%cwd%\\" %*) && popd
+  )
+)
